d414d6e46ec170d569ce00402a1892a697ace026
yaoyizhi <email@yaoadou888>
2017-04-18 10:56:38 +0800

    add attachment to resumes

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -2,4 +2,6 @@ class Resume < ApplicationRecord
     belongs_to :user
     belongs_to :job
     scope :recent, -> { order('created_at DESC') }
+    validates :content, presence: true
+    mount_uploader :attachment, AttachmentUploader
 end
